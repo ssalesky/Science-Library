85e0614cdff7cf5c1b408de52c80cbf243dae3fe
Scott Salesky <stsalesky@gmail.com>
2014-12-06 23:11:00 -0800

    Add public domain statement to header.

--- sci_lib.py
+++ sci_lib.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 #Author: Scott T. Salesky
-#Created: 12.6.2014
+#Email: scott.salesky@ubc.ca / stsalesky@gmail.com
+#Created: 12.6.2014 / Updated: 12.6.2014
 #Purpose: Collection of useful Python classes,
 #routines, and functions for scientific work
-#----------------------------------------------
+#----------------------------------------------------------
+#This is free software released into the public domain.
+#Anyone is free to copy, modify, publish, use, compile, sell, or
+#distribute this software, either in source code form or as a compiled
+#binary, for any purpose, commercial or non-commercial, and by any
+#means.
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+#OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+#ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+#OTHER DEALINGS IN THE SOFTWARE.
+#----------------------------------------------------------
 
 #Import all required packages
 import numpy as np
