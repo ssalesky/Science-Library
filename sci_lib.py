35f779cb56e4c710fb5321bd22d8187670993f62
Scott Salesky <ssalesky@Scotts-MacBook-Pro.local>
2014-12-06 21:22:29 -0800

    Add function to read 3d direct access Fortran binary files into NumPy arrays.

---
+++ sci_lib.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#Author: Scott T. Salesky
+#Created: 12.6.2014
+#Purpose: Collection of functions, routines to use 
+#Python for scientific work
+#----------------------------------------------
